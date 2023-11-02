184fab95518b7e02a6edad986e706835326cf408
kitou88C39 <kanfutrooper@icloud.com>
2023-11-02 15:37:55 +0900

    Program.cs 編集

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using dotnet_rpg.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet_rpg.Controllers
