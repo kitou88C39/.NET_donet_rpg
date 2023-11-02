54225a5d80add86746cb06d4a5c6fd898bf5893d
kitou88C39 <kanfutrooper@icloud.com>
2023-11-02 14:57:13 +0900

    Models/Character.cs 編集

--- Models/Character.cs
+++ Models/Character.cs
@@ -8,6 +8,6 @@ public class Character
     public int Strength { get; set; } = 10;
     public int Defense { get; set; } = 10;
     public int Intelligence { get; set; } = 10;
-    public int RpgClass class { get; set; }
+    public RpgClass Class { get; set; } = RpgClass.Knight;
 }
 }
