57412a13aeecb1a7d4819018349d751f1b05050b
kitou88C39 <kanfutrooper@icloud.com>
2023-11-04 13:41:54 +0900

    Services/CharacterService/ICharacterService.cs 編集

--- Services/CharacterService/ICharacterService.cs
+++ Services/CharacterService/ICharacterService.cs
@@ -7,6 +7,7 @@ namespace dotnet_rpg.Services.CharacterService
 {
     public interface ICharacterService
     {
-        
+        List<Character> GetAllCharacters();
+        Character GetCharacterById(int id);
     }
 }
