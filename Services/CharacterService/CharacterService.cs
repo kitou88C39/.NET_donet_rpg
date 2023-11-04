bd9b2f0f7644a2ee1ba5c5d029878fedb730e10d
kitou88C39 <kanfutrooper@icloud.com>
2023-11-04 13:51:18 +0900

    Services/CharacterService/CharacterService.cs 編集

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -5,8 +5,20 @@ using System.Threading.Tasks;
 
 namespace dotnet_rpg.Services.CharacterService
 {
-    public interface CharacterService
+    public interface CharacterService:ICharacterService
     {
-        
+        public List<Character> AddCharacter(Character newCharacter)
+        {
+            throw new NotImplementedException();
+        }
+        public List<Character> GetAllCharacter()
+        {
+            throw new NotImplementedException();
+        }
+        public Character GetCharacterById (int id)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
