32e1a275c64846c29f6ced2db326df7866a15267
kitou88C39 <kanfutrooper@icloud.com>
2023-11-09 14:30:16 +0900

    Models/ServiceResponse.cs 編集

--- Models/ServiceResponse.cs
+++ Models/ServiceResponse.cs
@@ -7,6 +7,8 @@ namespace dotnet_rpg.Models
 {
     public class ServiceResponse
     {
-        
+        public T? Data {get; set;}
+        public bool Success {get; set;} = true;
+        public string Message {get; set;} = string.Empty;
     }
 }
